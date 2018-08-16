ec8f4b502048ab6dd653da15844f38ff26b06d8e
Alpha Wong <alpha1916@163.com>
2018-08-16 15:57:18 +0800

    cc

--- sweetballs/main.7c617.js
+++ sweetballs/main.7c617.js
@@ -152,7 +152,7 @@
     // jsList
     let jsList = settings.jsList;
 
-    let bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.38f09.js';
+    let bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.acddb.js';
       if (jsList) {
         jsList = jsList.map(function (x) {
           return 'src/' + x;
@@ -187,7 +187,7 @@
 
 
   if (window.jsb) {
-    require('src/settings.52930.js');
+    require('src/settings.081e0.js');
     require('src/jsb_polyfill.js');
     boot();
     return;
