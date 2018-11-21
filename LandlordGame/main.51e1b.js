6dd7678475330e21abfea485b87cdc0153e00557
Alpha Wong <alpha1916@163.com>
2018-11-21 23:17:13 +0800

    cc
    增加游戏结束

--- LandlordGame/main.51e1b.js
+++ LandlordGame/main.51e1b.js
@@ -153,7 +153,7 @@
         var jsList = settings.jsList;
 
         if (!false) {
-            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.c3eea.js';
+            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.befd5.js';
             if (jsList) {
                 jsList = jsList.map(function (x) {
                     return 'src/' + x;
@@ -210,7 +210,7 @@
     }
 
     if (window.jsb) {
-        require('src/settings.eabbc.js');
+        require('src/settings.55430.js');
         require('src/jsb_polyfill.js');
         boot();
         return;
