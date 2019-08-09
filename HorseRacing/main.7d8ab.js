3b11369f94d321744f23f6f9de9c27ba0b8a85ed
Alpha Wong <alpha1916@163.com>
2019-08-09 22:41:01 +0800

    cc

--- HorseRacing/main.7d8ab.js
+++ HorseRacing/main.7d8ab.js
@@ -138,7 +138,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8a4bb.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.7a0e1.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -174,7 +174,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.98edf.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.7d8ab.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -200,12 +200,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.d412e.js');
+        require('src/settings.275ee.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.d412e.js');
+        require('src/settings.275ee.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
